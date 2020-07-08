eec929afbe9b62b89b064e11482caaa515ec0882
Tim Murray <timmurray@google.com>
2020-07-08 15:22:42 +0530

    mm: skip swap readahead when process is exiting
    It's possible for a user fault to be triggered during task exit that
    results in swap readahead, which is not useful. Skip swap readahead
    if the current process is exiting.
    
    Change-Id: I5fad20ebdcc616af732254705726d395eb118cbe
    Signed-off-by: Tim Murray <timmurray@google.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -490,6 +490,10 @@ struct page *swapin_readahead(swp_entry_t entry, gfp_t gfp_mask,
 	mask = is_swap_fast(entry) ? 0 : swapin_nr_pages(offset) - 1;
 	if (!mask)
 		goto skip;
+	
+	/* If exiting, don't do swap readahead. */
+	if (current->flags & PF_EXITING)
+		goto skip;
 
 	/* If exiting, don't do swap readahead. */
 	if (current->flags & PF_EXITING)
